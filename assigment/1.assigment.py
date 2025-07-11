762390c4af260c2c51a6ba9b3a7492989779399f
Mohith <chalamalasettimohith@gmail.com>
2025-07-11 13:09:45 +0530

    Update 1.assigment.py

--- assigment/1.assigment.py
+++ assigment/1.assigment.py
@@ -2,18 +2,18 @@
 order_id=int(input("Enter a Order ID: "))
 customer_name=str(input("Enter customer name: "))
 customer_number=int(input("Enter mobile number: "))
-bevarges_name=input("Enter bevarges name: ")
+beverages_name=input("Enter bevarges name: ")
 add_ons =list(input("Enter Items Ordered with quantity (format - item:qty, separated by commas): ").split(","))
 price=int(input("Enter price: ₹"))
 discount_percentage = float(input("Enter Discount Percentage: "))
 coupons=int(input("Enter the number of coupons: ")
-avilable_discount=int(input("Enter number of available discounts: "))
-discount_details=(coupons,availabile_discounts)
+available_discount=int(input("Enter number of available discounts: "))
+discount_details=(coupons,availabile_discount)
 customer_location = input(" Enter deliver location: ")
 delivery_time = set(input("How much time you want delivery: "))
 payment=input("Payment will be Online or cash: ")
 order_details={
-    'Customer ordered' : bevarges_name,
+    'Customer ordered' : beverages_name,
     'Customer add-ons' : add_ons,
     'Payment' : payment
 }
@@ -28,7 +28,7 @@ print("OrderID, Customer Name, customer_number: "+ str(order_id), customer_name,
 print("discount_Percentage: %.2f%%" % discount_percentage)
 
 #using f-strings(f"")
-print(f"Customer Name: {customer_name} \nCustomer nuber Contact No: {customer_number} \nPrice: ₹{price} \nDiscount Persentage: {discount_percentage}%")
+print(f"Customer Name: {customer_name} \nCustomer number Contact No: {customer_number} \nPrice: ₹{price} \nDiscount Percentage: {discount_percentage}%")
 
 #using.format()
 print("Order Details: Customer ordered - {}, Customer add-ons -{},bill payment".format(order_details["Customer ordered"],order_details["Customer add-ons"],order_details["Payment"]))
